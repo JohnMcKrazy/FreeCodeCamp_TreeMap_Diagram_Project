b57f544c1e4bfcd566509a8a56801af831f7e798
John GC <gc.john.dcg@gmail.com>
2023-09-20 10:38:10 -0600

    feat --> grill and cells set, next --> set legend

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     //! GLOBAL FUNCTIONS !//
-
+    //^ DELETE CHILDREN ^//
     const deleteChildElements = (parentElement) => {
         let child = parentElement.lastElementChild;
         while (child) {
@@ -8,9 +8,18 @@ document.addEventListener("DOMContentLoaded", () => {
             child = parentElement.lastElementChild;
         }
     };
+    //^ DELETE ARRAY ^//
+    const deleteArrElements = (parentElement) => {
+        while (parentElement.length > 0) {
+            parentElement.forEach((item) => {
+                parentElement.pop(item);
+            });
+        }
+    };
+
+    const fader = (color) => d3.interpolateRgb(color, "#fff")(0.2);
     //! GLOBAL DATA  !//
     //^ VARIANTS - CONSTANTS  ^//
-
     const body = document.querySelector("BODY");
     const dataBtnTemplate = document.querySelector("#data_btn_template").content;
     const searchNav = document.querySelector(".search_nav");
@@ -34,6 +43,14 @@ document.addEventListener("DOMContentLoaded", () => {
             api_link: "https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/kickstarter-funding-data.json",
         },
     ];
+    const mapDimentions = {
+        width: 960,
+        height: 570,
+        padding: 2,
+    };
+    const colorRange = ["#1f77b4", "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c", "#98df8a", "#d62728", "#ff9896", "#9467bd", "#c5b0d5", "#8c564b", "#c49c94", "#e377c2", "#f7b6d2", "#7f7f7f", "#c7c7c7", "#bcbd22", "#dbdb8d", "#17becf", "#9edae5"].map(fader);
+
+    const color = d3.scaleOrdinal().range(colorRange);
     //^CREATING DATA BTNS ^//
     SRC_DATA.forEach((item) => {
         const newTemplate = dataBtnTemplate.cloneNode(true);
@@ -44,11 +61,11 @@ document.addEventListener("DOMContentLoaded", () => {
         newBtn.setAttribute("data-db", item.key);
         searchNav.append(newBtn);
     });
+
     //^CREATING DESCRIPTION ^//
-
     const descriptionSection = d3.select("BODY").append("section").attr("id", "description_section").attr("class", "description_section flex flex_column_center");
-    //^ CREATE TITLE  ^//
 
+    //^ CREATE TITLE  ^//
     const titleMap = descriptionSection.append("h1").attr("id", "title").attr("class", "title_map");
     const descriptionMap = descriptionSection.append("h2").attr("id", "descrition").attr("class", "description_map");
     const createTitle = (data) => {
@@ -64,11 +81,69 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     };
 
+    //^ SET SVG  ^//
+    //~ SET SVG CONTAINER ~//
+    const mapSection = d3.select("BODY").append("section").attr("id", "map_section").attr("class", "map_section flex flex_column_center");
     //^ FETCH DATA   ^//
+    let rawData = {};
     const fetchData = async (link) => {
-        const rawData = await d3.json(link);
+        deleteChildElements(document.querySelector("#map_section"));
+        rawData = await d3.json(link);
         console.log(rawData);
         createTitle(rawData);
+
+        //~ SET SVG~//
+        const mapSVG = mapSection.append("svg").attr("class", "treemap").attr("id", "treemap").attr("width", mapDimentions.width).attr("height", mapDimentions.height);
+        //^ SET TREEMAP AND CONDITIONS  ^//
+        //~ SET TREEMAP ~//
+        const treemap = d3.treemap().size([mapDimentions.width, mapDimentions.height]).paddingInner(mapDimentions.padding);
+        //~ SET TREEMAP ROOT~//
+
+        const root = d3
+            .hierarchy(rawData)
+            .eachBefore((d) => {
+                return (d.data.id = (d.parent ? `${d.parent.data.id}.` : "") + d.data.name);
+            })
+            .sum((d) => d.value)
+            .sort((a, b) => b.height - a.height || b.value - a.value);
+
+        //~SET TREEMAP~//
+        treemap(root);
+        console.log(root.leaves());
+        //^ SET TREEMAP CELLS  ^//
+        const grill = mapSVG
+            .selectAll("g")
+            .data(root.leaves())
+            .enter()
+            .append("g")
+            .attr("class", "group")
+            .attr("transform", (d) => `translate(${d.x0},${d.y0})`);
+
+        grill
+            .append("rect")
+            .attr("id", (d) => d.data.id)
+            .attr("class", "tile")
+            .attr("width", (d) => d.x1 - d.x0)
+            .attr("height", (d) => d.y1 - d.y0)
+            .attr("data-name", (d) => d.data.name)
+            .attr("data-category", (d) => d.data.category)
+            .attr("data-value", (d) => d.data.value)
+            .attr("fill", (d) => color(d.data.category));
+
+        grill
+            .append("text")
+            .attr("class", "tile-text")
+            .selectAll("tspan")
+            .data((d) => d.data.name.split(/(?=[A-Z][^A-Z])/g))
+            .enter()
+            .append("tspan")
+            .attr("x", 4)
+            .attr("y", (d, i) => 13 + i * 10)
+            .text((d) => d);
+
+        //^ SET CATEGORIES ^//
+        let mapCategories = root.leaves().map((nodes) => nodes.data.category);
+        mapCategories = mapCategories.filter((category, index, self) => self.indexOf(category) === index);
     };
 
     //^ PROJECTING FIRST DATA ^//
